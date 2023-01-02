f4d90dfcfca0283718b6fdc9206d01e7517e1e01
Mickael Lamasuta <mickael.lamasuta@etu.unice.fr>
2023-01-02 16:26:48 +0100

    Gatling

---
+++ gatling/src/test/java/quarkus/jvm/QuarkusJvmSimulation.java
@@ -0,0 +1,39 @@
+package quarkus.jvm;
+
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.rampConcurrentUsers;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class QuarkusJvmSimulation extends Simulation {
+
+    ChainBuilder getHello = exec(
+            http("HelloController")
+                    .get("/hello")
+                    .check(status().is(200))
+    );
+
+    HttpProtocolBuilder httpProtocol =
+            http.baseUrl("http://localhost:8081")
+                    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+                    .acceptLanguageHeader("en-US,en;q=0.5")
+                    .acceptEncodingHeader("gzip, deflate")
+                    .userAgentHeader(
+                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
+                    );
+
+    ScenarioBuilder getHelloScenario = scenario("GetHello").exec(getHello);
+
+    {
+        setUp(
+                getHelloScenario.injectClosed(rampConcurrentUsers(100).to(5000).during(30))
+        ).protocols(httpProtocol);
+    }
+
+}
